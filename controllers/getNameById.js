2508e1c653bf3b48ea4e38c54f1441ca4c6a243b
Naman Khandelwal <namankhandelwal1607@gmail.com>
2024-10-29 00:23:24 +0530

    select operation

--- controllers/getNameById.js
+++ controllers/getNameById.js
@@ -5,13 +5,13 @@ const getNameById = async (req, res) => {
         const userId = req.params.userId; 
         console.log("User ID:", userId);
 
-        const reqUser = await User.findOne({ _id: userId });
+        const reqUser = await User.findOne({ _id: userId }).select("userName"); 
 
         if (!reqUser) {
             return res.status(404).json({ error: "User not found" });
         }
 
-        res.json({ user: reqUser });
+        res.json({ userName: reqUser.userName });
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: "An error occurred" });
