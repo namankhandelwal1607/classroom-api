6cbd40ee1c20f4c3539c7a63a56079eb0ba8e9bb
Naman Khandelwal <namankhandelwal1607@gmail.com>
2024-10-29 00:19:19 +0530

    await change

--- controllers/getNameById.js
+++ controllers/getNameById.js
@@ -1,13 +1,17 @@
 import { User } from "../models/userModel.js";
 
-const getNameById = async(req, res)=>{
-    try{
-        const userId = await req.params.userId;
-        console.log(userId);
+const getNameById = async (req, res) => {
+    try {
+        const userId = req.params.userId; 
+        console.log("User ID:", userId);
 
-        const reqUser = await User.findOne({_id: userId});
+        const reqUser = await User.findOne({ _id: userId });
 
-        res.json({reqUser});
+        if (!reqUser) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        res.json({ user: reqUser });
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: "An error occurred" });
