4be46bfa198b73f501e122678edc78912ed9d2c7
Naman Khandelwal <namankhandelwal47@gmail.com>
2024-11-02 12:35:18 +0530

    announcement mail done

--- controllers/announcement.js
+++ controllers/announcement.js
@@ -1,11 +1,10 @@
 import { Announcement } from "../models/announcementModel.js";
 import { User } from "../models/userModel.js";
 import { Class } from "../models/classModel.js";
-
+import sendMail from '../helpers/sendMail.js';
 const announcement = async (req, res) => {
     try {
         const { className, userName, announcement } = req.body;
-
         const user = await User.findOne({ _id: userName });
         const classn = await Class.findOne({ _id: className });
 
@@ -28,11 +27,21 @@ const announcement = async (req, res) => {
             userName,
             announcement
         });
-
+        console.log("sssssss", announcement);
         const savedAnnouncement = await newAnnouncement.save();
 
         classn.classAnnouncement.push(savedAnnouncement._id);
         await classn.save();
+    
+
+        for (const students of classn.classStudents) {
+            const stud = await User.findOne({_id: students});
+            const subject = `ClassConnect: New announcement in ${classn.className}`;
+            const text = `Dear ${stud.userName},\n\nNew Announcement in ${classn.className} from ${user.userName}:\n\n${savedAnnouncement.announcement}`;
+            await sendMail(stud.userName, subject, text);
+        }
+        
+       
 
         return res.status(201).json({
             message: 'Announcement created successfully',
@@ -48,3 +57,4 @@ const announcement = async (req, res) => {
 };
 
 export default announcement;
+
